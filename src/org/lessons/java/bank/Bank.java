b1fad07b2bc3ba7fa3ca56cc797d9aa338c56798
Antonio D'Angelo <reapercsp@gmail.com>
2023-10-20 17:47:55 +0200

    Add Bank and BankAccount

--- src/org/lessons/java/bank/Bank.java
+++ src/org/lessons/java/bank/Bank.java
@@ -3,10 +3,50 @@ package org.lessons.java.bank;
 import java.util.Scanner;
 
 public class Bank {
+
     public static void main(String[] args) {
 
         // Inizializzo lo Scanner
         Scanner input = new Scanner(System.in);
 
+        System.out.print("Inserisci il tuo nome: ");
+        String userName = input.nextLine();
+
+        // Creo un nuovo account
+        BankAccount userAccount = new BankAccount(userName);
+        System.out.println("Proprietario: " + userAccount.setOwner());
+        System.out.println("Numero di Conto: " + userAccount.getAccountNumber());
+        System.out.println("Saldo: " + userAccount.getBalance());
+
+        // Menu
+        while (true) {
+            System.out.println("Digita:");
+            System.out.println("1. Versare denaro");
+            System.out.println("2. Prelevare denaro");
+            System.out.println("3. Uscire");
+            System.out.print("Scegli un opzione per proseguire: ");
+
+            int choice = input.nextInt();
+
+            if (choice == 1) {
+                System.out.print("Inserisci l'importo da versare: ");
+                double depositAmount = input.nextDouble();
+                userAccount.deposit(depositAmount);
+                System.out.println("Saldo attuale: " + userAccount.getFormattedBalance());
+            } else if (choice == 2) {
+                System.out.print("Inserisci l'importo da prelevare: ");
+                double withdrawalAmount = input.nextDouble();
+                userAccount.withdraw(withdrawalAmount);
+                System.out.println("Saldo attuale: " + userAccount.getFormattedBalance());
+            } else if (choice == 3) {
+                System.out.println("Grazie per aver usato il nostro servizio.");
+                break;
+            } else {
+                System.out.println("Scelta non valida. Riprova.");
+            }
+        }
+
+        // Chiudo lo Scanner
+        input.close();
     }
 }
--- src/org/lessons/java/bank/BankAccount.java
+++ src/org/lessons/java/bank/BankAccount.java
@@ -1,32 +1,53 @@
 package org.lessons.java.bank;
+
+import java.util.Random;
+
 public class BankAccount {
 
     // Attributi
-    private double accountNumber;
-    private String owner;
+    private final int accountNumber;
+    private final String owner;
     private double balance;
 
     // Costruttore
-    public BankAccount(double accountNumber, String owner, double balance) {
-        this.accountNumber = accountNumber;
+    public BankAccount(String owner) {
+        this.accountNumber = generateAccountNumber();
         this.owner = owner;
-        this.balance = 0;
+        this.balance = 0.00;
     }
 
     // Metodi
-    public double getAccountNumber(){
+    public int getAccountNumber(){
         return this.accountNumber;
     }
-
     public String getOwner(){
         return this.owner;
     }
-
     public String setOwner(){
         return this.owner;
     }
-
     public double getBalance(){
         return this.balance;
+    }
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        } else {
+            System.out.println("L'importo del deposito deve essere maggiore di zero.");
+        }
+    }
+    public void withdraw(double amount) {
+        if (amount > 0 && balance >= amount) {
+            balance -= amount;
+        } else {
+            System.out.println("L'importo del prelievo non è valido.");
+        }
+    }
+    public String getFormattedBalance() {
+        return String.format("%.2f €", balance);
+    }
+    private int generateAccountNumber() {
+        Random rand = new Random();
+        return rand.nextInt(1000) + 1;
     }
 }
