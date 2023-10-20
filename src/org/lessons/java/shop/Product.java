ce348b1c283a5bb0d4c3d0d2794c48300ebc725e
Antonio D'Angelo <reapercsp@gmail.com>
2023-10-20 15:59:38 +0200

    Create Product

--- src/org/lessons/java/shop/Product.java
+++ src/org/lessons/java/shop/Product.java
@@ -1,25 +1,36 @@
 package org.lessons.java.shop;
 
+import java.util.Random;
+
 public class Product {
 
-    private int code;
+    // Attributi
+    private final int code;
     private String name;
     private String description;
-    private int price;
+    private double price;
     private int vat;
 
-    public Product(String name, String description, int price, int vat) {
+
+    // Costruttore
+    public Product(String name, String description, double price, int vat) {
+        Random rand = new Random();
+        this.code = rand.nextInt(20);
         this.name = name;
         this.description = description;
         this.price = price;
         this.vat = vat;
     }
 
+    // Metodi
     public int getCode(){
         return this.code;
     }
     public String getName(){
         return this.name;
+    }
+    public String completeName(){
+        return this.name + " " + this.code;
     }
     public void setName(String name){
         this.name = name;
@@ -30,14 +41,18 @@ public class Product {
     public void setDescription(String description){
         this.description = description;
     }
-    public int getPrice(){
+    public double getPrice(){
         return this.price;
     }
-    public void setPrice(int price){
+    public void setPrice(double price){
         this.price = price;
     }
     public int getVat(){
         return this.vat;
+    }
+    public double vatPrice() {
+        this.price = this.price * (1 + ((double) this.vat / 100));
+        return this.price;
     }
     public void setVat(int vat){
         this.vat = vat;
--- src/org/lessons/java/shop/Shop.java
+++ src/org/lessons/java/shop/Shop.java
@@ -1,7 +1,18 @@
 package org.lessons.java.shop;
 
+import java.util.Scanner;
+
 public class Shop {
     public static void main(String[] args) {
 
+        // Creo il nuovo Prodotto
+        Product pasta = new Product("Fusilli", "Pasta Corta", 10.0, 22);
+        System.out.println(pasta.getCode());
+        System.out.println(pasta.getName());
+        System.out.println(pasta.completeName());
+        System.out.println(pasta.getDescription());
+        System.out.println(pasta.getPrice());
+        System.out.println(pasta.vatPrice());
+        System.out.println(pasta.getVat());
     }
 }
